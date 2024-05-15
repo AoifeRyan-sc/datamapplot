a8aa7cf95492a7aba545baf206cfde7ecba1d95b
AoifeRyan-sc <125266574+AoifeRyan-sc@users.noreply.github.com>
2024-05-15 10:52:04 +0100

    added html output function for interactive graphs

--- datamapplot/create_plots.py
+++ datamapplot/create_plots.py
@@ -579,3 +579,279 @@ def create_interactive_plot(
     )
 
     return InteractiveFigure(html_str, width=width, height=height)
+
+def create_interactive_plot_html(
+    data_map_coords,
+    *label_layers,
+    hover_text=None,
+    inline_data=True,
+    noise_label="Unlabelled",
+    noise_color="#999999",
+    color_label_text=True,
+    label_wrap_width=16,
+    label_color_map=None,
+    width="100%",
+    height=800,
+    darkmode=False,
+    palette_hue_shift=0.0,
+    palette_hue_radius_dependence=1.0,
+    cmap=None,
+    marker_size_array=None,
+    marker_color_array=None,
+    use_medoids=False,
+    cluster_boundary_polygons=False,
+    color_cluster_boundaries=True,
+    polygon_alpha=0.1,
+    **render_html_kwds,
+):
+    """
+
+    Parameters
+    ----------
+    data_map_coords: ndarray of floats of shape (n_samples, 2)
+        The 2D coordinates for the data map. Usually this is produced via a
+        dimension reduction technique such as UMAP, t-SNE, PacMAP, PyMDE etc.
+
+    *label_layers: np.ndarray
+        All remaining positional arguments are assumed to be labels, each at
+        a different level of resolution. Ideally these should be ordered such that
+        the most fine-grained resolution is first, and the coarsest resolution is last.
+        The individual labels-layers should be formatted the same as for `create_plot`.
+
+    hover_text: list or np.ndarray or None (optional, default=None)
+        An iterable (usually a list of numpy array) of text strings, one for each
+        data point in `data_map_coords` that can be used in a tooltip when hovering
+        over points.
+
+    inline_data: bool (optional, default=True)
+        Whether to include data inline in the HTML file (compressed and base64 encoded)
+        of whether to write data to separate files that will then be referenced by the
+        HTML file -- in the latter case you will need to ensure all the files are
+        co-located and served over an http server or similar. Inline is the best
+        default choice for easy portability and simplicity, but can result in very
+        large file sizes.
+
+    noise_label: str (optional, default="Unlabelled")
+        The string used in the ``labels`` array to identify the unlabelled or noise points
+        in the dataset.
+
+    noise_color: str (optional, default="#999999")
+        The colour to use for unlabelled or noise points in the data map. This should usually
+        be a muted or neutral colour to distinguish background points from the labelled clusters.
+
+    color_label_text: bool (optional, default=True)
+        Whether to use colours for the text labels generated in the plot. If ``False`` then
+        the text labels will default to either black or white depending on ``darkmode``.
+
+    label_wrap_width: int (optional, default=16)
+        The number of characters to apply text-wrapping at when creating text labels for
+        display in the plot. Note that long words will not be broken, so you can choose
+        relatively small values if you want tight text-wrapping.
+
+    label_color_map: dict or None (optional, default=None)
+        A colour mapping to use to colour points/clusters in the data map. The mapping should
+        be keyed by the unique cluster labels in ``labels`` and take values that are hex-string
+        representations of colours. If ``None`` then a colour mapping will be auto-generated.
+
+    width: int or str (optional, default="100%")
+        The width of the plot when rendered in a notebook. This should be a valid HTML iframe
+        width specification -- either an integer number of pixels, or a string that can be
+        properly interpreted in HTML.
+
+    height: int or str (optional, default=800)
+        The height of the plot when rendered in a notebook. This should be a valid HTML iframe
+        height specification -- either an integer number of pixels, or a string that can be
+        properly interpreted in HTML.
+
+    darkmode: bool (optional, default=False)
+        Whether to render the plot in darkmode (with a dark background) or not.
+
+    palette_hue_shift: float (optional, default=0.0)
+        A setting, in degrees clockwise, to shift the hue channel when generating a colour
+        palette and color_mapping for the labels.
+
+    palette_hue_radius_dependence: float (optional, default=1.0)
+        A setting that determines how dependent on the radius the hue channel is. Larger
+        values will result in more hue variation where there are more outlying points.
+
+    cmap: matplotlib cmap or None (optional, default=None)
+        A linear matplotlib cmap colour map to use as the base for a generated colour mapping.
+        This *should* be a matplotlib cmap that is smooth and linear, and cyclic
+        (see the colorcet package for some good options). If not a cyclic cmap it will be
+        "made" cyclic by reflecting it. If ``None`` then a custom method will be used instead.
+
+    marker_size_array: np.ndarray or None (optional, default=None)
+        An array of sizes for each of the points in the data map scatterplot.
+
+    use_medoids: bool (optional, default=False)
+        Whether to use medoids instead of centroids to determine the "location" of the cluster,
+        both for the label indicator line, and for palette colouring. Note that medoids are
+        more computationally expensive, especially for large plots, so use with some caution.
+
+    cluster_boundary_polygons: bool (optional, default=False)
+        Whether to draw alpha-shape generated boundary lines around clusters. This can be useful
+        in highlighting clusters at different resolutions when using many different label_layers.
+
+    polygon_alpha: float (optional, default=0.1)
+        The alpha value to use when genrating alpha-shape based boundaries around clusters.
+
+    **render_html_kwds:
+        All other keyword arguments will be passed through the `render_html` function. Please
+        see the docstring of that function for further options that can control the
+        aesthetic results.
+
+    Returns
+    -------
+
+    """
+    if len(label_layers) == 0:
+        label_dataframe = pd.DataFrame(
+            {
+                "x": [data_map_coords.T[0].mean()],
+                "y": [data_map_coords.T[1].mean()],
+                "label": [""],
+                "size": [np.power(data_map_coords.shape[0], 0.25)],
+            }
+        )
+    else:
+        label_dataframe = pd.concat(
+            [
+                label_text_and_polygon_dataframes(
+                    labels,
+                    data_map_coords,
+                    noise_label=noise_label,
+                    use_medoids=use_medoids,
+                    cluster_polygons=cluster_boundary_polygons,
+                    alpha=polygon_alpha,
+                )
+                for labels in label_layers
+            ]
+        )
+
+    if label_color_map is None:
+        if cmap is None:
+            palette = palette_from_datamap(
+                data_map_coords,
+                label_dataframe[["x", "y"]].values,
+                hue_shift=palette_hue_shift,
+                radius_weight_power=palette_hue_radius_dependence,
+            )
+        else:
+            palette = palette_from_cmap_and_datamap(
+                cmap,
+                data_map_coords,
+                label_dataframe[["x", "y"]].values,
+                radius_weight_power=palette_hue_radius_dependence,
+            )
+        if not darkmode:
+            text_palette = np.asarray(
+                [
+                    tuple(int(c * 255) for c in to_rgb(color))
+                    for color in deep_palette(palette)
+                ]
+            )
+        else:
+            text_palette = np.asarray(
+                [
+                    tuple(int(c * 255) for c in to_rgb(color))
+                    for color in pastel_palette(palette)
+                ]
+            )
+        palette = [tuple(int(c * 255) for c in to_rgb(color)) for color in palette]
+        color_map = {
+            label: color for label, color in zip(label_dataframe.label, palette)
+        }
+    else:
+        color_map = {
+            label: tuple(int(c * 255) for c in to_rgb(color))
+            for label, color in label_color_map.items()
+        }
+        if not darkmode:
+            text_palette = np.asarray(
+                [
+                    tuple(int(c * 255) for c in to_rgb(color))
+                    for color in deep_palette(
+                        [label_color_map[label] for label in label_dataframe.label]
+                    )
+                ]
+            )
+        else:
+            text_palette = np.asarray(
+                [
+                    tuple(int(c * 255) for c in to_rgb(color))
+                    for color in pastel_palette(
+                        [label_color_map[label] for label in label_dataframe.label]
+                    )
+                ]
+            )
+
+    if color_label_text or color_cluster_boundaries:
+        label_dataframe["r"] = text_palette.T[0]
+        label_dataframe["g"] = text_palette.T[1]
+        label_dataframe["b"] = text_palette.T[2]
+        label_dataframe["a"] = 64
+    else:
+        label_dataframe["r"] = 15 if not darkmode else 240
+        label_dataframe["g"] = 15 if not darkmode else 240
+        label_dataframe["b"] = 15 if not darkmode else 240
+        label_dataframe["a"] = 64
+
+    label_dataframe["label"] = label_dataframe.label.map(
+        lambda x: textwrap.fill(x, width=label_wrap_width, break_long_words=False)
+    )
+
+    point_dataframe = pd.DataFrame(
+        {
+            "x": data_map_coords.T[0],
+            "y": data_map_coords.T[1],
+        }
+    )
+    if hover_text is not None:
+        point_dataframe["hover_text"] = hover_text
+
+    if marker_size_array is not None:
+        point_dataframe["size"] = marker_size_array
+
+    if marker_color_array is None:
+        color_vector = np.asarray(
+            [tuple(int(c * 255) for c in to_rgb(noise_color))]
+            * data_map_coords.shape[0],
+            dtype=np.uint8,
+        )
+        for labels in reversed(label_layers):
+            label_map = {n: i for i, n in enumerate(np.unique(labels))}
+            if noise_label not in label_map:
+                label_map[noise_label] = -1
+            label_unmap = {i: n for n, i in label_map.items()}
+            cluster_label_vector = np.asarray(pd.Series(labels).map(label_map))
+            unique_non_noise_labels = [
+                label for label in label_unmap if label != label_map[noise_label]
+            ]
+            for label in unique_non_noise_labels:
+                color_vector[cluster_label_vector == label] = color_map[
+                    label_unmap[label]
+                ]
+    else:
+        color_vector = np.asarray(
+            [
+                tuple(int(c * 255) for c in to_rgb(color))
+                for color in marker_color_array
+            ],
+            dtype=np.uint8,
+        )
+
+    point_dataframe["r"] = color_vector.T[0].astype(np.uint8)
+    point_dataframe["g"] = color_vector.T[1].astype(np.uint8)
+    point_dataframe["b"] = color_vector.T[2].astype(np.uint8)
+    point_dataframe["a"] = np.uint8(180)
+
+    html_str = render_html(
+        point_dataframe,
+        label_dataframe,
+        inline_data=inline_data,
+        color_label_text=color_label_text,
+        darkmode=darkmode,
+        **render_html_kwds,
+    )
+
+    return html_str
